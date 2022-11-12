35a3ec196e168211c183b03adca039ff528d03f7
chhotu1 <chhotukumarsow@gmail.com>
2022-11-12 23:21:42 +0530

    cashbook

--- src/Components/Dashboad/index.js
+++ src/Components/Dashboad/index.js
@@ -3,7 +3,7 @@ import { FaDollarSign, FaShoppingCart, FaUsers } from 'react-icons/fa'
 const Dashboard = ({ dashboard }) => {
     let credit = 0;
     let debit = 0;
-    dashboard && dashboard.data && dashboard.data.map((e) => {
+    dashboard && dashboard.data && dashboard.data.cashbook && dashboard.data.cashbook.map((e) => {
         if (e._id === 'CREDIT') {
             credit = e.total;
         } else {
@@ -121,87 +121,31 @@ const Dashboard = ({ dashboard }) => {
                                                     <tr>
                                                         <th scope="col">#</th>
                                                         <th scope="col">Customer</th>
-                                                        <th scope="col">Product</th>
+                                                        <th scope="col">Type</th>
                                                         <th scope="col">Price</th>
-                                                        <th scope="col">Status</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    <tr>
-                                                        <th scope="row">
-                                                            <a href="#">#2457</a>
-                                                        </th>
-                                                        <td>Brandon Jacob</td>
-                                                        <td>
-                                                            <a href="#" className="text-primary">
-                                                                At praesentium minu
-                                                            </a>
-                                                        </td>
-                                                        <td>$64</td>
-                                                        <td>
-                                                            <span className="badge bg-success">Approved</span>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">
-                                                            <a href="#">#2147</a>
-                                                        </th>
-                                                        <td>Bridie Kessler</td>
-                                                        <td>
-                                                            <a href="#" className="text-primary">
-                                                                Blanditiis dolor omnis similique
-                                                            </a>
-                                                        </td>
-                                                        <td>$47</td>
-                                                        <td>
-                                                            <span className="badge bg-warning">Pending</span>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">
-                                                            <a href="#">#2049</a>
-                                                        </th>
-                                                        <td>Ashleigh Langosh</td>
-                                                        <td>
-                                                            <a href="#" className="text-primary">
-                                                                At recusandae consectetur
-                                                            </a>
-                                                        </td>
-                                                        <td>$147</td>
-                                                        <td>
-                                                            <span className="badge bg-success">Approved</span>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">
-                                                            <a href="#">#2644</a>
-                                                        </th>
-                                                        <td>Angus Grady</td>
-                                                        <td>
-                                                            <a href="#" className="text-primar">
-                                                                Ut voluptatem id earum et
-                                                            </a>
-                                                        </td>
-                                                        <td>$67</td>
-                                                        <td>
-                                                            <span className="badge bg-danger">Rejected</span>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">
-                                                            <a href="#">#2644</a>
-                                                        </th>
-                                                        <td>Raheem Lehner</td>
-                                                        <td>
-                                                            <a href="#" className="text-primary">
-                                                                Sunt similique distinctio
-                                                            </a>
-                                                        </td>
-                                                        <td>$165</td>
-                                                        <td>
-                                                            <span className="badge bg-success">Approved</span>
-                                                        </td>
-                                                    </tr>
+                                                    {
+                                                        dashboard && dashboard.data && dashboard.data.cashbookUser && dashboard.data.cashbookUser.map((item,index)=>{
+                                                            return(
+                                                                <tr key={index}>
+                                                                <th scope="row">
+                                                                    <a href="#">{index+1}</a>
+                                                                </th>
+                                                                <td>{item.data[0]?.name}</td>
+                                                                <td>
+                                                                    <a href="#" className="text-primary">
+                                                                    {item.data[0].type}
+                                                                    </a>
+                                                                </td>
+                                                                <td>&#8377;{item?.totalPrice}</td>
+                                                                
+                                                            </tr>
+
+                                                            )
+                                                        })
+                                                    }
                                                 </tbody>
                                             </table>
                                         </div>
