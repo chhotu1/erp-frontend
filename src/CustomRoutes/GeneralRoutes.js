c86f1bd584592fa728936938c1429d5e62fbeb7f
chhotu1 <chhotukumarsow@gmail.com>
2022-11-14 07:35:51 +0530

    login route protected

--- src/CustomRoutes/GeneralRoutes.js
+++ src/CustomRoutes/GeneralRoutes.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Login from '../Pages/Login';
+import StorageService from '../services/StorageService';
+import AuthRoutes from './AuthRoutes';
 import RouteName from './RouteName';
 
 const GeneralRoutes = () => {
     return (
         <Routes>
-            <Route path={RouteName.LOGIN} name="login" element={<Login />} />
+            <Route element={<AuthRoutes isLoggedIn={StorageService.getAccessToken()} />}>
+                <Route path={RouteName.LOGIN} name="login" element={<Login />} />
+            </Route>
         </Routes>
     )
 }
--- src/utils/screenEndPoints.js
+++ src/utils/screenEndPoints.js
@@ -1,6 +1,7 @@
 const screenEndPoints = {
     login: '/public/login',
     register: '/public/register',
+    home: '/',
   };
   
   export default screenEndPoints;
